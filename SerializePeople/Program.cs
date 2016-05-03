17f816c30daf44fb25e70f53ee7824301ff4e60b
Vegh Adam <veghadam1991@gmail.com>
2016-05-03 09:26:40 +0200

    added exercise 1: make a class serializable

---
+++ SerializePeople/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SerializePeople
+{
+	[Serializable]
+	public class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public double ID { get; set; }
+
+		public Person()
+		{
+		}
+
+		public Person(string name, int age, double id)
+		{
+			Name = name;
+			Age = age;
+			ID = id;
+		}
+
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person me = new Person("Adam", 25, 1234567890);
+		}
+	}
+}
